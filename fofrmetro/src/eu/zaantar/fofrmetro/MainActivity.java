aa3bdef727e5c8a7a5090c5dc1c9a0b6a98cab0b
Zbyněk Jiráček <JBI@seznam.cz>
2014-11-30 15:05:51 +0100

    tried to load xml data from xml/data.xml, but it's not working

--- fofrmetro/src/eu/zaantar/fofrmetro/MainActivity.java
+++ fofrmetro/src/eu/zaantar/fofrmetro/MainActivity.java
@@ -1,11 +1,14 @@
 package eu.zaantar.fofrmetro;
 
+import javax.xml.xpath.XPathExpressionException;
+
 import android.support.v7.app.ActionBarActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -13,10 +16,20 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends ActionBarActivity {
 
+	private MetroContext metroContext;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        try {
+        	java.io.InputStream stream = getApplicationContext().getResources().openRawResource(R.xml.data);
+        	metroContext = new MetroContext(stream);
+        } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+        }
         
         // toggle listener for metroLineSelection
         ((RadioGroup) findViewById(R.id.metroLineSelection)).setOnCheckedChangeListener(ToggleListener);
@@ -53,13 +66,25 @@ public class MainActivity extends ActionBarActivity {
         toast.show();*/
     }
     
-    static final RadioGroup.OnCheckedChangeListener ToggleListener = new RadioGroup.OnCheckedChangeListener() {
+    final RadioGroup.OnCheckedChangeListener ToggleListener = new RadioGroup.OnCheckedChangeListener() {
 		@Override
 		public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
 			for (int j = 0; j < radioGroup.getChildCount(); j++) {
                 final ToggleButton view = (ToggleButton) radioGroup.getChildAt(j);
                 view.setChecked(view.getId() == checkedId);
             }
+				        
+			ToggleButton checkedButton = (ToggleButton) findViewById(radioGroup.getCheckedRadioButtonId());
+	        try {
+				String[] directions = metroContext.getDirectionsForLine(checkedButton.getText().toString());
+				RadioButton direction1 = (RadioButton) findViewById(R.id.direction1Radio);
+				RadioButton direction2 = (RadioButton) findViewById(R.id.direction2Radio);
+				direction1.setText(directions[0]);
+				direction2.setText(directions[1]);
+			} catch (XPathExpressionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	};
 }
--- fofrmetro/src/eu/zaantar/fofrmetro/MetroContext.java
+++ fofrmetro/src/eu/zaantar/fofrmetro/MetroContext.java
@@ -1,16 +1,20 @@
 package eu.zaantar.fofrmetro;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 
 public class MetroContext {
@@ -30,7 +34,7 @@ public class MetroContext {
 	private Document resourceDoc;	
 	private XPath xp;
 	
-	public MetroContext(InputStream resourceInputStream) throws Exception {
+	public MetroContext(InputStream resourceInputStream) throws ParserConfigurationException, SAXException, IOException {
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db = dbf.newDocumentBuilder();
 		resourceDoc = db.parse(resourceInputStream);
@@ -38,14 +42,14 @@ public class MetroContext {
 		xp = XPathFactory.newInstance().newXPath();
 	}
 	
-	public String[] getDirectionsForLine(String lineName) throws Exception {
+	public String[] getDirectionsForLine(String lineName) throws XPathExpressionException {
 		NodeList directionNodes = (NodeList) xp.evaluate("/metro/line[@name='" + lineName + "']/direction", resourceDoc, XPathConstants.NODESET);
 		for(int i = 0; i < directionNodes.getLength(); ++i) {
 			Node directionNode = directionNodes.item(i);
 			
 		}
 		
-		return null;
+		return new String[] { lineName + ": Tam", lineName + ": Zpìt" };
 	}
 
 }
